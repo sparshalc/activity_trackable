254c5d87315e5f6cda7f2e924ee99478e0d3fa7e
sparshalc <imailsparsha@gmail.com>
2025-06-04 00:38:02 +0545

    Add tracking status banner

---
+++ app/controllers/company_settings_controller.rb
@@ -0,0 +1,27 @@
+class CompanySettingsController < ApplicationController
+  before_action :set_company_setting
+
+  def show;  end
+
+  def edit; end
+
+  def update
+    if @company_setting.update(company_setting_params)
+      redirect_to company_settings_path, notice: "Settings updated successfully!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_company_setting
+    @company = ActsAsTenant.current_tenant
+
+    @company_setting = @company.company_setting
+  end
+
+  def company_setting_params
+    params.require(:company_setting).permit(:activity_tracking, :activity_retention_days)
+  end
+end
